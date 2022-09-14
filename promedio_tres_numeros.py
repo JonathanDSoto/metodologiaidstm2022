9f8a03fc1f5950c56c2416239e469e86cc8871ea
Jonathan Soto <jonathansoto@MacBook-Pro-de-Jonathan.local>
2022-09-14 07:08:13 -0600

    prom

---
+++ promedio_tres_numeros.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 14 07:04:32 2022
+
+@author: jonathansoto
+"""
+
+#declaración 
+num1 = 0
+num2 = 0
+num3 = 0
+prom = 0
+
+#lectura de datos  
+
+num1 = int( input("Ingrese el num 1 \n") )
+num2 = int( input("Ingrese el num 2 \n") )
+num3 = int( input("Ingrese el num 3 \n") )
+
+#operaciones
+prom = ( num1 + num2 + num3 ) / 3
+
+print("El resultado es: ", prom)
+
