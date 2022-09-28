79f764ca121e25fcfa1b8e4b491cf1148d6f891d
Jonathan Soto <jonathansoto@MacBook-Pro-de-Jonathan.local>
2022-09-28 07:25:47 -0600

    for

---
+++ numeros_n_al_100.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 14 07:04:32 2022
+
+@author: jonathansoto
+"""
+#declaración
+n = 0
+
+#lectura de datos 
+n = int( input("Ingrese N: \n") )
+
+#operaciones  
+   
+if(n>100):
+    for i in range(n,99,-1):
+        print(i)
+    
+if(n<100):
+    for i in range(n,101):
+        print(i)
+    
+    
+    
